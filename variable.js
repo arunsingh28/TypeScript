a73c81643cfb3e16d5c9d492c41ea6a213878701
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-23 00:54:23 +0530

    define variable

---
+++ variable.js
@@ -0,0 +1,7 @@
+var variable = "world";
+console.log(variable);
+// function
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("Arun", 'Singh'));
---
+++ variable.ts
@@ -0,0 +1,14 @@
+let variable: String = "world"
+
+console.log(variable)
+
+
+// function
+
+
+const getFullName = (name: String, surname:String) => {
+    return name + " " + surname
+}
+
+
+console.log(getFullName("Arun",'Singh'))
